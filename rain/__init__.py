a6b44afa7549de10973273b6d84b6b0986235c68
K Richard Pixley <rich@noir.com>
2013-12-18 15:01:09 -0800

    placeholders

---
+++ rain/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Time-stamp: <18-Dec-2013 15:00:04 PST by rich@noir.com>
+
+# Copyright © 2013 K Richard Pixley
+
+"""
+"""
+
+__docformat__ = 'restructuredtext en'
+
+__all__ = [
+]
+
---
+++ rain/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Time-stamp: <18-Dec-2013 14:54:55 PST by rich@noir.com>
+
+# Copyright © 2013 K Richard Pixley
+
+"""
+Shell callable driver for the :py:mod:`rain` library.
+"""
+
+import argparse
+import fnmatch
+import logging
+import os
+import re
+
+#import rain
+
+__docformat__ = "restructuredtext en"
+
+def main():
+    pass
