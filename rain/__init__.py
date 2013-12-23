2e066329196146c766f3558510e33af25c4de164
K Richard Pixley <rich@noir.com>
2013-12-23 14:36:28 -0800

    coelesce

--- rain/__init__.py
+++ rain/__init__.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Time-stamp: <18-Dec-2013 15:00:04 PST by rich@noir.com>
+# Time-stamp: <23-Dec-2013 14:35:49 PST by rich@noir.com>
 
 # Copyright © 2013 K Richard Pixley
 
-"""
-"""
+'''
+Class representing disk locations.
+'''
 
 __docformat__ = 'restructuredtext en'
 
 __all__ = [
 ]
 
+class DiskLocation:
+    locationName = None
+
+    def __init__(self, locationName=None):
+        self.locationName = locationName
+
+    def new_workspace(self):
+        
