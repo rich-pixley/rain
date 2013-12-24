5a1ad0937ccf92b72aa77ed9d67488bcc056bc51
K Richard Pixley <rich@noir.com>
2013-12-23 16:22:23 -0800

    evolution

--- rain/__init__.py
+++ rain/__init__.py
@@ -1,24 +1,84 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Time-stamp: <23-Dec-2013 14:35:49 PST by rich@noir.com>
+# Time-stamp: <23-Dec-2013 15:17:32 PST by rich@noir.com>
 
 # Copyright © 2013 K Richard Pixley
 
-'''
+"""
 Class representing disk locations.
-'''
+
+.todo: workspace as context manager?
+"""
 
 __docformat__ = 'restructuredtext en'
 
 __all__ = [
 ]
 
-class DiskLocation:
-    locationName = None
+import os
+import shutil
+import logging
 
-    def __init__(self, locationName=None):
-        self.locationName = locationName
+logger = logging.getLogger(__name__)
 
-    def new_workspace(self):
-        
+class RainException(Exception):
+    """Base class for all :py:mod:`rain` exceptions"""
+    pass
+
+
+class WorkSpace:
+    """Very simple workspace object."""
+
+    name = None
+    logger = None
+
+    def __init__(self, name=None, logger=logger):
+        self.name = name
+        self.logger = logger
+
+    def create(self):
+        """called to create the work space"""
+        self.logger.log(logging.DEBUG, 'os.mkdir %s', self.name)
+        os.mkdir(self.name)
+
+    def remove(self):
+        self.logger.log(logging.DEBUG, 'shutil.rmtree %s', self.name)
+        shutil.rmtree(self.name)
+
+
+class AllocationError(RainException):
+    """Raised when we can't allocate a workspace."""
+    pass
+
+
+class Location:
+    '''
+    Brain dead allocator.  Assume local disk, single work space.
+    '''
+
+    name = None
+    workspace = None
+
+    def __init__(self, name='.', logger=logger):
+        self.name = name
+        self.workspace = None
+
+    def next_workspace(self):
+        if self.workspace:
+            raise AllocationError
+
+        self.workspace = WorkSpace(os.path.join(self.name, 'WorkSpace'))
+        self.workspace.create()
+        return self.workspace
+
+    def remove_workspace(self, workspace):
+        if workspace is not self.workspace:
+            raise AllocationError
+
+        self.workspace.remove()
+        self.workspace = None
+
+
+def do_pass():
+    
--- rain/main.py
+++ rain/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Time-stamp: <23-Dec-2013 14:19:20 PST by rich@noir.com>
+# Time-stamp: <23-Dec-2013 16:14:41 PST by rich@noir.com>
 
 # Copyright © 2013 K Richard Pixley
 
@@ -14,10 +14,40 @@ import fnmatch
 import logging
 import os
 import re
+import contextlib
+import subprocess
+import shlex
 
-#import rain
+import rain
 
 __docformat__ = "restructuredtext en"
 
+@contextlib.contextmanager
+def pushdir(newdir):
+    savedir = os.getcwd()
+    os.chdir(newdir)
+    yield
+
+    os.chdir(savedir)
+
+class EmacsBuild:
+    @staticmethod
+    def build(workspace):
+        try:
+            with pushdir(workspace.name):
+                subprocess.check_call(shlex.split('tar xfJ /home/rich/local/src/emacs-24.3.tar.xz'))
+                with pushdir('emacs-24.3'):
+                    subprocess.check_call(shlex.split('./configure --with-x-toolkit=no --with-xpm=no --with-jpeg=no --with-png=no --with-gif=no --with-tiff=no'))
+                    subprocess.check_call(shlex.split('time make -j4'))
+
+        except:
+            return False
+
+        return True
+
 def main():
-    pass
+    location = Location('.')
+    workspace = location.next_workspace()
+    
+if __name__ == '__main__':
+    main()
